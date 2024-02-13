2654e6616ff11451488baa6fccde5b494c58aeb2
Dibi Hembram <dibihembram4@gmail.com>
2024-02-13 11:52:41 +0530

    map

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -39,7 +39,9 @@ function Footer(){
                     
                </div>
            </div>
-           <div className="part3"></div>
+           <div className="part3">
+            <iframe src="https://www.google.com/maps/place/Ruchika+Market,+Baramunda+Housing+Board+Colony,+Baramunda,+Bhubaneswar,+Odisha+751003/@20.2774277,85.7926044,18z/data=!3m1!4b1!4m15!1m8!3m7!1s0x3a1909d2d5170aa5:0xfc580e2b68b33fa8!2sBhubaneswar,+Odisha!3b1!8m2!3d20.2960587!4d85.8245398!16zL20vMDNjenFz!3m5!1s0x3a19a788fefbb1e9:0xe8410d6618eb0f1f!8m2!3d20.2772756!4d85.7951726!16s%2Fg%2F11cn6z5w5k?entry=ttu" className="map"/>
+           </div>
        </div>
     </>
     );
